20f5b479c39ff255ba0657071dc60766be9663bb
balkis hmidi <balkis.hmidi@esprit.tn>
2023-12-08 20:00:18 +0100

    nn

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ReservationModalComponent } from './Etudiant/components/reservation-mod
     AddetudiantComponent,
     StatisticetudiantComponent,
     ReservationModalComponent,
+   
     
 
     
